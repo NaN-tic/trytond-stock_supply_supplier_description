1482f97b9f7e76d2115c1448214dafcae69fa652
resteve <resteve@zikzakmedia.com>
2014-04-24 15:10:02 +0200

    pyflakes

--- product.py
+++ product.py
@@ -1,8 +1,7 @@
 # This file is part of the stock_supply_supplier_description module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import Pool, PoolMeta
-from trytond.transaction import Transaction
+from trytond.pool import PoolMeta
 
 __all__ = ['ProductSupplier']
 __metaclass__ = PoolMeta
