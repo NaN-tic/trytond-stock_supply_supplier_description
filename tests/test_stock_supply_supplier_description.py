e453d1e95fcdf94d5eb7aadd6e6a9bda080f5f83
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:49:18 +0100

    Cleanup tests

--- tests/test_stock_supply_supplier_description.py
+++ tests/test_stock_supply_supplier_description.py
@@ -7,25 +7,17 @@ import os
 import sys
 import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class StockSupplySupplierDescriptionTestCase(unittest.TestCase):
-    '''
-    Test Stock Supply Supplier Description module.
-    '''
+    'Test Stock Supply Supplier Description module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module(
             'stock_supply_supplier_description')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
