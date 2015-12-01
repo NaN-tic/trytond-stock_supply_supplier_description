82afde0f161a140368ef2f4d2d222247884e0b74
resteve <resteve@zikzakmedia.com>
2015-12-01 01:03:38 +0100

    Tests

--- tests/test_stock_supply_supplier_description.py
+++ tests/test_stock_supply_supplier_description.py
@@ -1,24 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the stock_supply_supplier_description module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends
-import os
-import sys
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockSupplySupplierDescriptionTestCase(unittest.TestCase):
+class StockSupplySupplierDescriptionTestCase(ModuleTestCase):
     'Test Stock Supply Supplier Description module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'stock_supply_supplier_description')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_supply_supplier_description'
 
 
 def suite():
@@ -26,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         StockSupplySupplierDescriptionTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
